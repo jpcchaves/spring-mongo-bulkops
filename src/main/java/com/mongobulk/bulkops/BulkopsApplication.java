a14a5081eb2d76e357dfd41aa737e5546055bf8e
jpcchaves <jpcchaves@outlook.com>
2024-11-02 14:27:45 -0300

    spring boot init

---
+++ src/main/java/com/mongobulk/bulkops/BulkopsApplication.java
@@ -0,0 +1,13 @@
+package com.mongobulk.bulkops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkopsApplication.class, args);
+	}
+
+}
